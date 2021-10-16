f8e98aa3d000ec782addf968ed0b03bb74a889cb
Biraj Adhikari <adhikari.biraj1@gmail.com>
2021-10-15 23:23:43 -0500

    running program and training neural net separated

---
+++ runCartPole.py
@@ -0,0 +1,42 @@
+import gym
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+def update_scene(num, frames, patch):
+    patch.set_data(frames[num])
+    return patch,
+
+def plot_animation(frames, repeat=False, interval=40):
+    fig = plt.figure()
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+    anim = animation.FuncAnimation(
+        fig, update_scene, fargs=(frames, patch),
+        frames=len(frames), repeat=repeat, interval=interval)
+    plt.close()
+    return anim
+
+def render_policy_net(model, n_max_steps=200, seed=42):
+    frames = []
+    env = gym.make("CartPole-v1")
+    env.seed(seed)
+    np.random.seed(seed)
+    obs = env.reset()
+    for step in range(n_max_steps):
+        frames.append(env.render(mode="rgb_array"))
+        left_proba = model.predict(obs.reshape(1, -1))
+        action = int(np.random.rand() > left_proba)
+        obs, reward, done, info = env.step(action)
+        if done:
+            break
+    env.close()
+    return frames
+import os
+
+model = keras.models.load_model(os.getcwd())
+frames = render_policy_net(model)
+plot_animation(frames)
